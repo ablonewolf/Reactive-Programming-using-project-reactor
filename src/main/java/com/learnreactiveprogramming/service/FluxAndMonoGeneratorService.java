cde7657efa0a10b875598d9f04eb2da5c0efc9f1
Arka Bhuiyan <arka.bhuiyan@bjitgroup.com>
2023-02-09 14:47:05 +0600

    performed map on a flux and a mono.

--- src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -15,6 +15,21 @@ public class FluxAndMonoGeneratorService {
         return Mono.just("Arka Bhuiyan").log();
     }
 
+    public Mono<String> monoMap() {
+        return Mono
+                .just("Arka Bhuiyan")
+                .map(String::toUpperCase)
+                .log();
+    }
+
+    public Flux<String> nameFlux_map() {
+        return Flux
+                .fromIterable(List.of("Arka","Farhan","Akif","Nipa","Zareen","Mosfikur"))
+                .filter(name -> name.length() > 4)
+                .map(String::toUpperCase)
+                .log();
+    }
+
     public static void main(String[] args) {
         FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
 //        subscribing to the name flux
--- src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,4 +1,46 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 public class FluxAndMonoGeneratorServiceTest {
+
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+    @Test
+    void namesFlux() {
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux();
+
+        StepVerifier.create(namesFlux)
+                .expectNext("Arka")
+                .expectNextCount(5)
+                .verifyComplete();
+    }
+
+    @Test
+    void nameMono() {
+        var nameMono = fluxAndMonoGeneratorService.nameMono();
+
+        StepVerifier.create(nameMono)
+                .expectNext("Arka Bhuiyan")
+                .verifyComplete();
+    }
+
+    @Test
+    void nameFlux_map() {
+        var nameFluxMap = fluxAndMonoGeneratorService.nameFlux_map();
+
+        StepVerifier.create(nameFluxMap)
+                .expectNext("FARHAN")
+                .expectNextCount(2)
+                .verifyComplete();
+    }
+
+    @Test
+    void monoMap() {
+        var monoMap = fluxAndMonoGeneratorService.monoMap();
+
+        StepVerifier.create(monoMap)
+                .expectNext("ARKA BHUIYAN")
+                .verifyComplete();
+    }
 }
